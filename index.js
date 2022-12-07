0e50e387c95b49a2edd29dd3043f956b7294923b
Beatriz Vidal <biavidalf@gmail.com>
2022-12-06 23:48:58 -0300

    Ajustando porta

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ app.use(
     }),
 );
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.json());
 app.use('', router);
+
+const PORT = process.env.PORT || 3333;
 
 const DB_USER = 'vidal-databaseNumber1';
 const DB_PASSWORD = process.env.pass_database1;
@@ -23,7 +25,7 @@ mongoose
   .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ip1c0xp.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => {
     console.log('Conectado');
-    app.listen(3333);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log(err);
